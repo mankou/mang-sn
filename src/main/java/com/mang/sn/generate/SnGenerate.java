bc0f8978f62ee19ad13701e887dd9d1a7a4dd047
mang <ningma003@gmail.com>
2017-06-25 16:43:24 +0800

    * 修改注释

--- src/main/java/com/mang/sn/generate/SnGenerate.java
+++ src/main/java/com/mang/sn/generate/SnGenerate.java
@@ -2,6 +2,19 @@ package com.mang.sn.generate;
 
 import java.util.Map;
 
+/**
+ * 单号生成
+ * */
 public interface SnGenerate {
-	public String generateSn(String prefix,Long maxIndex,Map<String,Object> map);
+	
+	/**
+	 * 生成具体的单号
+	 * 
+	 * @param prefix 单号前缀
+	 * @param maxIndex 单号序号
+	 * @param paraMap 其它传口实现类的参数 用于扩展
+	 * 
+	 * @return 单号
+	 * */
+	public String generateSn(String prefix,Long maxIndex,Map<String,Object> paraMap);
 }
